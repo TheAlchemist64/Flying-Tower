0423b18c3f77edd0212e256b1bc848d5367145db
Daniel S <TheAlchemist64@users.noreply.github.com>
2017-07-03 14:37:18 -0500

    Add Gulpfile and install vinyl-source-stream

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+/**
+* Gulpfile to make my life easier.
+*/
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+
+// Lets bring es6 to es5 with this.
+// Babel - converts ES6 code to ES5 - however it doesn't handle imports.
+// Browserify - crawls your code for dependencies and packages them up 
+// into one file. can have plugins.
+// Babelify - a babel plugin for browserify, to make browserify 
+// handle es6 including imports.
+gulp.task('bundle', function() {
+	browserify({
+    	entries: './scripts/game.js',
+    	debug: true
+  	})
+    .transform('babelify',{presets: ['env']})
+    .on('error',gutil.log)
+    .bundle()
+    .on('error',gutil.log)
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('watch',function() {
+	gulp.watch('./scripts/*.js',['bundle'])
+});
+ 
+gulp.task('default', ['watch']);
