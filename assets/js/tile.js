d6909b62e695ed69c7dec3204cdfaa40bddc12bf
Daniel S <TheAlchemist64@users.noreply.github.com>
2017-07-18 11:18:34 -0500

    Add Foreground property to Tile class
    Makes the foreground color of tiles not hard-coded.

--- assets/js/tile.js
+++ assets/js/tile.js
@@ -4,12 +4,13 @@ import Game from './game';
 export let TileTypes = new Enum('EMPTY','FLOOR','WALL');
 
 export class Tile {
-	constructor(x, y, chr){
+	constructor(x, y, chr, fg){
 		this.x = x;
 		this.y = y;
 		this.chr = chr;
+		this.fg = fg;
 	}
 	draw(){
-		Game.display.draw(this.x, this.y, this.chr, '#fff');
+		Game.display.draw(this.x, this.y, this.chr, this.fg);
 	}
 }
