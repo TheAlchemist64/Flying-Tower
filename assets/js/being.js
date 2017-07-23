f05665562749ec2c5b744c172f044db74d73ac80
Daniel S <TheAlchemist64@users.noreply.github.com>
2017-07-23 14:43:49 -0500

    Create Being class

---
+++ assets/js/being.js
@@ -0,0 +1,14 @@
+import Game from './game'
+
+export class Being {
+	constructor(name, x, y){
+		this.name = name;
+		this.x = x;
+		this.y = y;
+	}
+	act(){}
+}
+
+export class Player extends Being{
+	
+}
