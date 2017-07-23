e3353655dd51b6beaf026858dfe42a1364d805f4
Daniel S <TheAlchemist64@users.noreply.github.com>
2017-07-23 14:48:10 -0500

    Add EventBus to Game object

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,5 @@
-import ROT from '../../vendor/rot'
+import ROT from '../../vendor/rot';
+import { EventBus } from '../../vendor/eventbus.min';
 
 const w = 50;
 const h = 25;
@@ -6,6 +7,7 @@ const h = 25;
 export default {
 	display: null,
 	map: null,
+	bus: null,
 	
 	init: function(){
 		this.display = new ROT.Display({width: w, height: h});
@@ -15,5 +17,7 @@ export default {
 		this.map.create((x, y, wall)=>{
 			this.display.draw(x, y, wall ? '#': '.');
 		});
+		
+		this.bus = EventBus;
 	}
 }
