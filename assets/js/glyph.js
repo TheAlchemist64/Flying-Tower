581a42287953031ecadd0d136b66386a5ef1d9fb
Daniel S <TheAlchemist64@users.noreply.github.com>
2017-07-24 14:39:28 -0500

    Remove former getters/setters from Glyph

--- assets/js/glyph.js
+++ assets/js/glyph.js
@@ -6,12 +6,4 @@ export default class Glyph {
 		this.fg = fg || null;
 		this.bg = bg || null;
 	}
-	//Setters and Getters for properties
-	//When any of those properties are updated, the console redraws the glyph
-	/*get chr(){ return this._chr; }
-	set chr(chr){ this._chr = chr; Game.bus.dispatch('redraw',this); }
-	get fg(){ return this._fg; }
-	set fg(fg){ this._fg = fg; this.draw(); }
-	get bg(){ return this._bg; }
-	set bg(bg){ this._bg = bg; this.draw(); }*/
 }
