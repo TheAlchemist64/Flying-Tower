98e3aa63d790ba7076cb6e41136d2308fecd884c
Daniel S <TheAlchemist64@users.noreply.github.com>
2017-07-25 19:44:44 -0500

    Remove unecessary arrow function

--- assets/js/game.js
+++ assets/js/game.js
@@ -49,9 +49,7 @@ export default {
 		
 		this.bus.addEventListener('in-bounds',this.map.inBounds,this.map);
 		
-		this.bus.addEventListener('reset',(e, x, y)=>{
-			this.map.reset(e, x, y);
-		},this.map);
+		this.bus.addEventListener('reset', this.map.reset, this.map);
 		
 		this.bus.addEventListener('fall',(e)=>{
 			this.map.reset(e,e.target.x,e.target.y);
