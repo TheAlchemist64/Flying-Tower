461c639e42a97dd50df8baef3a9a7dc695219341
Daniel S <TheAlchemist64@users.noreply.github.com>
2017-07-26 18:13:34 -0500

    Allow AI to push the player off tower
    If the player is standing next to a sky tile, the Basic AI will make
    their way to the player and push them off.

--- assets/js/ai/basic.js
+++ assets/js/ai/basic.js
@@ -1,16 +1,49 @@
 import ROT from '../../../vendor/rot';
 import Game from '../game';
 
-function isPassable(x, y){
-	return !['wall','sky'].includes(Game.map.get(x, y).type);
+function isPassable(x, y, actor){
+	let passable = true;
+	if(['wall','sky'].includes(Game.map.get(x, y).type)){
+		passable = false;
+	}
+	let [collides, other] = actor.collides(x, y);
+	if(collides){
+		passable = false;
+	}
+	return passable;
 }
 
 class BasicAI {
 	constructor(){
-		
+		this.finder = null;
+		this.path = [];
 	}
 	run(actor){
-		
+		let [result, tile] = Game.player.canFall();
+		if(!result){
+			return;
+		}
+		//Get the tile the AI needs to be on in order to push the player off
+		let x = Game.player.x - (tile.x - Game.player.x);
+		let y = Game.player.y - (tile.y - Game.player.y);
+		//Make passable function callback
+		let passableCallback = function(x, y){
+			let result = isPassable(x, y, actor);
+			return result;
+		}
+		//Initialize pathfinder
+		this.finder = new ROT.Path.AStar(x, y, passableCallback, {topology:4});
+		//Find path to tile where ai can push the player off
+		this.path = [];
+		this.finder.compute(actor.x, actor.y, (x, y)=>{
+			this.path.push({x: x, y: y});
+		});
+		if(this.path.length == 1){
+			actor.move(Game.player.x, Game.player.y);
+		}
+		else if(this.path.length > 1){
+			actor.move(this.path[1].x, this.path[1].y);
+		}
 	}
 }
 
