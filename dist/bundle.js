22d0aeb1b46583ce01bb97781642988daedee706
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2017-09-18 20:09:16 -0500

    Fix infinite loop bug in collapser

--- dist/bundle.js
+++ dist/bundle.js
@@ -5612,14 +5612,14 @@ class Collapser{
 	collapseTileGroup(tiles){
 		tiles.forEach(tile => this.collapseTile(tile.x, tile.y));
 	}
-	isExitReachable(){
+	getPathToExit(){
 		let passable = (x, y) => Game.map.get(x, y).type != "sky";
 		let astar = new rot.Path.AStar(Game.exit[0], Game.exit[1], passable, {topology: 4});
 		let path = [];
 		astar.compute(Game.player.x, Game.player.y, (x, y) => {
 			path.push([x, y]);
 		});
-		return path.length > 0;
+		return path;
 	}
 	checkConnections(map){
 		
@@ -5629,12 +5629,12 @@ class Collapser{
 			this.delay--;
 		}
 		else{
-			while(true){
+			while(Object.keys(Game.map.floors).length > this.getPathToExit().length){
 				let pick = randFloor(Game.map);
 				if(pick!=null){
 					let tmp = Game.map.get(...pick);
 					this.collapseTile(...pick);
-					if(this.isExitReachable()){
+					if(this.getPathToExit().length > 0){
 						Game.map.get(...pick).draw();
 						break;
 					}
