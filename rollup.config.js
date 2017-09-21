157db9084e84f3c425b126d821b0e87c19385fee
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2017-09-20 19:41:07 -0500

    Install local-resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import commonjs from 'rollup-plugin-commonjs';
+import localResolve from 'rollup-plugin-local-resolve';
 
 export default {
 	entry: 'src/app.js',
 	format: 'iife',
-	plugins: [ commonjs() ],
+	plugins: [ localResolve(), commonjs() ],
 	dest: 'dist/bundle.js',
 	sourceMap: true
 }
