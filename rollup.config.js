5606e2c105b2688671991f840a3cd972fe97488d
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2017-09-20 20:01:53 -0500

    Revert "Install local-resolve plugin"
    This reverts commit 157db9084e84f3c425b126d821b0e87c19385fee.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 import commonjs from 'rollup-plugin-commonjs';
-import localResolve from 'rollup-plugin-local-resolve';
 
 export default {
 	entry: 'src/app.js',
 	format: 'iife',
-	plugins: [ localResolve(), commonjs() ],
+	plugins: [ commonjs() ],
 	dest: 'dist/bundle.js',
 	sourceMap: true
 }
