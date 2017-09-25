23b453cc51016bace9257342605c42492a825150
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2017-09-25 12:08:48 -0500

    Make Collapser less uniform in how it picks tiles

--- src/collapser.js
+++ src/collapser.js
@@ -5,10 +5,15 @@ import Game, { randTile, randFloor } from "./game";
 import Tile from "./map/tile";
 import TileTypes from './map/tiletypes';
 
+const notBetweenPnE = 2;
+const notOnPath = 3;
+const canBeFatal = 4;
+
 export default class Collapser{
 	constructor(map, delay){
 		this.map = map;
 		this.delay = delay || 0; // # of turns to wait before collapsing tiles
+		this.steps = 0;
 		Game.scheduler.add(this,true);
 	}
 	collapseTile(x, y){
@@ -39,40 +44,89 @@ export default class Collapser{
 		this.updateConnections(map, x, y+1);
 		this.updateConnections(map, x-1, y);
 	}
+	betweenPlayerAndExit(x, y){
+		let dx = this.map.exit[0] - Game.player.x;
+		let dy = this.map.exit[1] - Game.player.y;
+		let innerProduct = (x - Game.player.x) * dx + (y - Game.player.y) * dy;
+		return 0 <= innerProduct && innerProduct <= dx*dx + dy*dy;
+	}
+	collapseSection(){
+		Object.keys(this.map.floors).forEach(floor => {
+			let [x,y] = floor.split(',');
+			if(!this.map.get(x, y).connected){
+				this.collapseTile(x, y);
+				this.map.get(x, y).draw();
+			}
+		});
+	}
+	collapseSectionNotOnPath(){
+		while(Object.keys(this.map.floors).length > this.getPathToExit().length){
+			let pick = randFloor(this.map);
+			if(pick!=null){
+				let tmp = this.map.get(...pick);
+				this.collapseTile(...pick);
+				if(this.getPathToExit().length > 0){
+					delete this.map.floors[pick];
+					this.map.get(...pick).draw();
+					this.map.tiles.forEach((tile,k)=>{
+						tile.connected = false;
+					});
+					this.updateConnections(this.map, this.map.exit[0], this.map.exit[1]);
+					this.collapseSection();
+					break;
+				}
+				else{
+					this.map.set(tmp);
+				}
+			}
+			else if(Object.keys(this.map.floors).length == 0){
+				break;
+			}
+		}
+	}
 	act(){
 		if(this.delay > 0){
 			this.delay--;
 			bus.dispatch('tickCollapseTimer', this, this.delay);
 		}
 		else{
-			while(Object.keys(this.map.floors).length > this.getPathToExit().length){
+			this.steps++;
+			if(this.steps % canBeFatal == 0){
+				//any tile can be collapsed, which can be fatal to the player
 				let pick = randFloor(this.map);
 				if(pick!=null){
-					let tmp = this.map.get(...pick);
 					this.collapseTile(...pick);
-					if(this.getPathToExit().length > 0){
-						delete this.map.floors[pick];
-						this.map.get(...pick).draw();
-						this.map.tiles.forEach((tile,k)=>{
-							tile.connected = false;
-						});
-						this.updateConnections(this.map, this.map.exit[0], this.map.exit[1]);
-						Object.keys(this.map.floors).forEach(floor => {
-							let [x,y] = floor.split(',');
-							if(!this.map.get(x, y).connected){
-								this.collapseTile(x, y);
-								this.map.get(x, y).draw();
-							}
-						});
-						break;
-					}
-					else{
-						this.map.set(tmp);
+					delete this.map.floors[pick];
+					this.map.get(...pick).draw();
+					this.map.tiles.forEach((tile,k)=>{
+						tile.connected = false;
+					});
+					this.updateConnections(this.map, this.map.exit[0], this.map.exit[1]);
+					this.collapseSection();
+					if(this.map.get(Game.player.x, Game.player.y).type=='sky'){
+						Game.over(false);
 					}
 				}
-				else if(Object.keys(this.map.floors).length == 0){
-					break;
+			}
+			else if(this.steps % notOnPath == 0){
+				//the tile collapsed cannot be on the shortest path to the exit
+				this.collapseSectionNotOnPath();
+			}
+			else if(this.steps % notBetweenPnE == 0){
+				//The tile cannot be between the player and the exit at all.
+				let pick = null;
+				while(pick==null || this.betweenPlayerAndExit(...pick)){
+					pick = randFloor(this.map);
+					
 				}
+				this.collapseTile(...pick);
+				delete this.map.floors[pick];
+				this.map.get(...pick).draw();
+				this.map.tiles.forEach((tile,k)=>{
+					tile.connected = false;
+				});
+				this.updateConnections(this.map, this.map.exit[0], this.map.exit[1]);
+				this.collapseSection();
 			}
 		}
 	}
