ad8a1c2c8aa00f317911ac70ccc7389f86a21b2e
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2017-12-05 14:01:29 -0600

    Remove status code in Controller.run()

--- src/controller.js
+++ src/controller.js
@@ -1,27 +1,3 @@
 export default class Controller {
-	run(actor){
-		if(this.state=="stunned"){
-			this.stunned--;
-			if(this.stunned > 0){
-				this.glyph.chr = this.stunned;
-			}
-			else{
-				this.state = "immune";
-				this.immune = 1;
-				this.glyph.chr = "*";
-			}
-			this.draw();
-		}
-		else if(this.state=="immune"){
-			this.immune--;
-			if(!this.immune){
-				this.state = "active";
-				this.glyph.chr = this._chr;
-				delete this._chr;
-				this.glyph.fg = this._fg;
-				delete this._fg;
-				this.draw();
-			}
-		}
-	}
+	run(actor){}
 }
