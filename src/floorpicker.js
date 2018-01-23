7b41062b428b0c083ef10a63aeb4873cdd89eeb0
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-01-23 16:28:11 -0600

    Make queue that FloorPicker uses private

--- src/floorpicker.js
+++ src/floorpicker.js
@@ -1,21 +1,22 @@
 import ROT from '../vendor/rot';
 import PriorityQueue from '../vendor/priority-queue.min';
 
+let queue = new PriorityQueue({
+  comparator: (a,b) => Math.floor(ROT.RNG.getUniform() * 3) - 1
+});
+
 export default {
-  queue: new PriorityQueue({
-  	comparator: (a,b) => Math.floor(ROT.RNG.getUniform() * 3) - 1
-  }),
   empty(){
-    return this.queue.length == 0;
+    return queue.length == 0;
   },
   pick() {
-    return this.queue.dequeue();
+    return queue.dequeue();
   },
   put(tile) {
-    this.queue.queue(tile.x+','+tile.y);
+    queue.queue(tile.x+','+tile.y);
   },
   setMap(map){
-    this.queue.clear();
+    queue.clear();
     Array.from(map.tiles.values()).filter(
       tile => tile.type == "floor"
     ).forEach(tile => this.put(tile));
