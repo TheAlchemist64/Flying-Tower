628c78994a0baba9f82b42dfb349beb39ff9577e
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-01-30 19:45:05 -0600

    Remove old unused "AI" source files

--- src/ai/stun.js
+++
@@ -1,9 +0,0 @@
-import Game from '../game';
-import BasicAI from './basic';
-
-export default class StunnerAI extends BasicAI{
-	run(actor) {
-		let path = this.findPath(actor, Game.player.x, Game.player.y);
-		this.moveToPlayer(actor, path);
-	}
-}
