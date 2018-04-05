46c5fb34a21170fcf25b1405003066c9ff61b5c0
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-04-04 19:54:00 -0500

    Fix bug where sentinels didn't stayed red after they got out of range

--- src/controllers/sentinel.js
+++ src/controllers/sentinel.js
@@ -9,7 +9,9 @@ export default class SentinelController extends Controller {
     super.run(actor);
     if(distance(actor.x, actor.y, Game.player.x, Game.player.y) < 5){
       //Change color
-      actor.glyph.fg = 'red';
+      if(actor.glyph.fg != 'red'){
+        actor.glyph.fg = 'red';
+      }
       //Initialize pathfinder
   		let finder = new ROT.Path.AStar(Game.player.x, Game.player.y, passable, {topology:4});
       //let finder = new ROT.Path.AStar(Game.map.exit[0], Game.map.exit[1], passable, {topology:4});
