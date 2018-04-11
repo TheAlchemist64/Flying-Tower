19653b9941fffc34ef94387753ee2e6d29965c5e
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-04-11 18:12:13 -0500

    Awake Sentinels when path to player is up to five tiles away
    Previously, Sentinels awoke if the distance was five tiles away. This
    meant it was possible for the player ad sentinel to be in seperate
    areas, yet the player would still wake up the sentinel. Use A* instead
    of distance formula to find how far sentinel is from player.

--- src/controllers/sentinel.js
+++ src/controllers/sentinel.js
@@ -7,19 +7,18 @@ import Controller from '../controller';
 export default class SentinelController extends Controller {
   run(actor){
     super.run(actor);
-    if(distance(actor.x, actor.y, Game.player.x, Game.player.y) < 5){
+    //Initialize pathfinder
+    let finder = new ROT.Path.AStar(Game.player.x, Game.player.y, passable, {topology:4});
+    //Find path from AI to player
+    let path = [];
+    finder.compute(actor.x, actor.y, (x, y)=>{
+      path.push({x: x, y: y});
+    });
+    if(path.length <= 5){
       //Change color
       if(actor.glyph.fg != 'red'){
         actor.glyph.fg = 'red';
       }
-      //Initialize pathfinder
-  		let finder = new ROT.Path.AStar(Game.player.x, Game.player.y, passable, {topology:4});
-      //let finder = new ROT.Path.AStar(Game.map.exit[0], Game.map.exit[1], passable, {topology:4});
-      //Find path from AI to player
-  		let path = [];
-  		finder.compute(actor.x, actor.y, (x, y)=>{
-  			path.push({x: x, y: y});
-  		});
       //Move onto player's tile
       if(path.length > 1){
   			actor.move(path[1].x, path[1].y);
